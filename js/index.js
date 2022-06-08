3ed519ab51b0a15bd2215c7c41387f3e0e9ead5d
Apache-Duster <mikelopezca@gmail.com>
2022-06-08 09:45:48 +0200

    js added

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// password gen upon a user input
+const box = document.querySelector(".box");
+
+const changeBg = () => {
+  box.style.backgroundColor = "orange";
+};
+box.addEventListener("click", changeBg);// password gen upon a user input
+const box = document.querySelector(".box");
+
+const changeBg = () => {
+  box.style.backgroundColor = "orange";
+};
+box.addEventListener("click", changeBg);
